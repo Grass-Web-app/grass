ffd874f4efc5dc3e026c72a82412ad259ca86099
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-02-27 18:48:48 -0600

    new proves

--- src/pages/description/[id].tsx
+++ src/pages/description/[id].tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import CardsOfPics from "../../../app/components/grass/CardsOfPics";
-import CarouselGrass from "../../../app/components/grass/CarouselGrass";
-import FullDescription from "../../../app/components/grass/FullDescription";
-import GrassHeader from "../../../app/components/grass/GrassHeader";
-import { DivGrassDescription } from "../../../app/components/grass/styledgrass";
-import HeaderFooterWraper from "../../../app/components/layout/InitWraper";
+import CardsOfPics from "../../app/components/grass/CardsOfPics";
+import CarouselGrass from "../../app/components/grass/CarouselGrass";
+import FullDescription from "../../app/components/grass/FullDescription";
+import GrassHeader from "../../app/components/grass/GrassHeader";
+import { DivGrassDescription } from "../../app/components/grass/styledgrass";
+import HeaderFooterWraper from "../../app/components/layout/InitWraper";
 
 const index = () => {
-  const { query: routeQuery, asPath, push } = useRouter();
+  const { query: routeQuery } = useRouter();
   const [Id, setId] = useState<any>(0);
   useEffect(() => {
     if (routeQuery && routeQuery?.id) {
