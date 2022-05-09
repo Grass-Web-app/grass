535cfd6bdcf6e30fb6e288c4b3662e4875ed8139
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-05-08 21:28:48 -0500

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,36 @@
 import React, { useEffect } from "react";
-import { useRouter } from "next/router";
+import ShowHeader from "../app/components/header/ShowHeader";
+import CardsBottom from "../app/components/home/CardsBottom";
+import Carousel from "../app/components/home/Carousel";
+import Description from "../app/components/home/Description";
+import Engineered from "../app/components/home/Engineered";
+import GridHover from "../app/components/home/GridHover";
+import HeaderFooterWraper from "../app/components/layout/InitWraper";
+import { DivContainerFull } from "../app/components/home/styledIndex";
+import ShowBack from "../app/components/home/ShowBack";
+import Footer from "../app/components/footer/Footer";
 
 const index = () => {
-  const { push } = useRouter();
   useEffect(() => {
-    push("/home");
+    if (localStorage.getItem("FustadesingCatalogue") == null) {
+      localStorage.setItem("FustadesingCatalogue", "");
+    }
   }, []);
 
-  return <></>;
+  return (
+    <HeaderFooterWraper show={false}>
+      <DivContainerFull>
+        <ShowHeader />
+        <Description />
+        <GridHover />
+        <Engineered />
+        <ShowBack />
+        <Carousel />
+        <CardsBottom />
+        <Footer />
+      </DivContainerFull>
+    </HeaderFooterWraper>
+  );
 };
 
 export default index;
